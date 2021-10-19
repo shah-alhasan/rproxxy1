ca3f7af9c01bfe86aa1480b5bca0a1fcfee023ed
shah <shah@gmail.com>
2021-10-19 23:26:57 +0000

    shahalhasan

---
+++ precache-manifest.3fcffdb9af49921f4d363d7b371126fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c661c40471671cf2d46a0def3d34d3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f09f00b15728dc043ee3",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c833ac33215bb47a4641",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f09f00b15728dc043ee3",
+    "url": "/static/js/2.634b7570.chunk.js"
+  },
+  {
+    "revision": "d81c6c91afd2bb4bf8caccddf0472235",
+    "url": "/static/js/2.634b7570.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c833ac33215bb47a4641",
+    "url": "/static/js/main.16fa8821.chunk.js"
+  },
+  {
+    "revision": "f799113acf86ede46e25",
+    "url": "/static/js/runtime-main.56699e5a.js"
+  }
+]);
